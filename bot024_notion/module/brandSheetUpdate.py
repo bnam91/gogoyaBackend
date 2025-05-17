4b6486a41ba503cb0c52d591a6f7bbd4360965a5
bnam91 <darling_91@naver.com>
2025-05-18 02:12:35 +0900

    업데이트2

--- bot024_notion/module/brandSheetUpdate.py
+++ bot024_notion/module/brandSheetUpdate.py
@@ -9,108 +9,11 @@
 해야될거.
 
 공구인원 리체크 -> 사람체크할 수 있게
-공구인원 기존 진행상품 보완 -> 이거먼저 진행행
-공구인원 별 셀러아이템 찾기
+
 
 노션에 프린트 및 팝업되게 만들기
 공구문구 확인하기
 
 
 
-
-
-
-
-
-
-공구인원 기존 진행상품 보완 -> 이거먼저 진행
-
-
-1. 몽고db에 어떤식으로 크롤링 결과를 어떻게 저장이 됐는지 확인
-
-
-insta09_database.02_main_influencer_data
-
-brand
-Array (2)
-
-0
-Object
-name
-"쉐프윈"
-category
-""
-
-products
-Array (1)
-
-0
-Object
-item
-"쉐프윈 316 드림핸즈"
-type
-"공구오픈"
-category
-"🍽주방용품&식기"
-category2
-"기타"
-mentioned_date
-"2025-04-10T01:00:07.000Z"
-expected_date
-"2025-04-10"
-end_date
-""
-item_feed_link
-"https://www.instagram.com/p/DIPzCazx5sG/"
-preserve
-""
-
-1
-Object
-name
-"555"
-category
-""
-
-products
-Array (1)
-
-0
-Object
-item
-"엑스트라버진올리브오일"
-type
-"공구오픈"
-category
-"🥦식품&건강식품"
-category2
-"간편식&조미료"
-mentioned_date
-"2025-04-24T01:00:08.000Z"
-expected_date
-""
-end_date
-""
-item_feed_link
-"https://www.instagram.com/p/DIz2JMaRpS-/"
-preserve
-""
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 '''
-
